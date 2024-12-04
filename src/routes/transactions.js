1f4ddb4b3980631a5934d3af965896e430ded9e8
Pratik4230 <pratikjadhav1438@gmail.com>
2024-12-04 09:18:20 +0530

    routing of bar chart , pie chart and combined response

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -5,9 +5,17 @@ const router = Router();
 import {
   getAllTransactions,
   getStatistics,
+  getBarChart,
+  getPieChart,
 } from "../controllers/transactions.js";
+
+import combinedResponse from "../controllers/combinedResponse.js";
 
 router.route("/feed").get(getAllTransactions);
 router.route("/statistics").get(getStatistics);
+router.route("/barchart").get(getBarChart);
+router.route("/piechart").get(getPieChart);
+
+router.route("/combinedresponse").get(combinedResponse);
 
 export default router;
