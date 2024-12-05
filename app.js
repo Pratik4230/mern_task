f49dd038f671ec5628629ca7d8f7923ceb19abde
Pratik4230 <pratikjadhav1438@gmail.com>
2024-12-05 13:27:51 +0530

    configured cors origin

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  })
+);
 
 import seeddata from "./src/routes/seed_data.js";
 import transactions from "./src/routes/transactions.js";
